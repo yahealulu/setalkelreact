15cc617727f9ca1cdbc42b7ca74739d8c4eae3e1
yahealulu <108096100+yahealulu@users.noreply.github.com>
2025-07-01 12:52:03 +0300

    Fix i18n request scope error

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,17 +1,7 @@
 import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
  
-export default getRequestConfig(async ({requestLocale}) => {
-  // This typically corresponds to the `[locale]` segment
-  let locale = await requestLocale;
- 
-  // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale)) {
-    locale = routing.defaultLocale;
-  }
-  // console.log('Requested locale:', locale);
+export default getRequestConfig(async ({locale}) => {
   return {
-    locale,
     messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
